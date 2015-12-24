109251ea887d959149ba40f8cdb4a4cd5141fa4d
Florian Wahl <florian.wahl@hotmail.fr>
2015-12-24 15:35:11 +0100

    update 0.18 : disable zoom and scrolling on iOS

--- header.php
+++ header.php
@@ -3,9 +3,15 @@
     <title>POLO - Air France</title>
 
     <!-- Include meta tag to ensure proper rendering and touch zooming -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <!-- Au dessus : désactive le zoom
+    En dessous : désactive les on touch move (zoom, pinch, scrolling) -->
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script>
+        document.ontouchmove = function(event){
+            event.preventDefault();
+        }
+    </script>
 
     <link rel="manifest" href="/manifest.json">
 
